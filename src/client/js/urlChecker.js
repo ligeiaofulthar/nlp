8333dd7140b02eb06be734c9c8cc9ebe5fd861fe
ligeiaofulthar <office@juliahalper.at>
2021-02-23 19:13:16 +0100

    add: src

--- src/client/js/urlChecker.js
+++ src/client/js/urlChecker.js
@@ -1,6 +1,7 @@
 function checkUrl(inputText) {
     console.log("::: Running checkForName :::", inputText);
 
+    // https://stackoverflow.com/questions/30970068/js-regex-url-validation
     var regex = inputText.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
 
     if(regex.includes(inputText)) {
