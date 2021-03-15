ea67fcee0e11627fc9ca1c0d0c3d85f12b0dab3b
ligeiaofulthar <office@juliahalper.at>
2021-03-15 19:34:04 +0100

    fix: path of fetchData to localhost:5000

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -8,39 +8,36 @@ function handleSubmit(event) {
     console.log("::: Form Submitted :::", event, textUrl)
 
     if (checkUrl(textUrl)) {
-        fetchData('/sentiment', textUrl)
-    // .then(res => res.json())
-    .then(res => {
-        document.getElementById('score_tag').innerHTML = 'Polarity: '+polarity(res.score_tag);
-        document.getElementById('irony').innerHTML = `Irony: ${res.irony}`;
-        document.getElementById('confidence').innerHTML = `Confidence: ${res.confidence}`;
-        document.getElementById('subjectivity').innerHTML = `Subjectivity: ${res.subjectivity}`;
-        document.getElementById('text').innerHTML = `Url: ${res.url}`;
+        fetchData('http://localhost:5000/sentiment', textUrl)
+        .then(res => {
+            document.getElementById('score_tag').innerHTML = 'Polarity: '+polarity(res.score_tag);
+            document.getElementById('irony').innerHTML = `Irony: ${res.irony}`;
+            document.getElementById('confidence').innerHTML = `Confidence: ${res.confidence}`;
+            document.getElementById('subjectivity').innerHTML = `Subjectivity: ${res.subjectivity}`;
+            document.getElementById('text').innerHTML = `Url: ${res.url}`;
 
-        const badge = document.getElementsByClassName('badge--selector');
-        for (var i=0; i<badge.length; i++) {
-            badge[i].classList.add("badge");
-        }
-    })
-}
+            const badge = document.getElementsByClassName('badge--selector');
+            for (var i=0; i<badge.length; i++) {
+                badge[i].classList.add("badge");
+            }
+        })
+    }
 };
 
 const fetchData = async(textUrl = {},  data = {}) => {
-    const resp = await fetch('/sentiment', {
+    const resp = await fetch(textUrl, {
     method: 'POST',
     mode: 'cors',
     credentials: 'same-origin',
-    // cache: "no-cache",
     headers: {
         'Content-Type': 'application/json',
-        // 'Accept': 'application/json'
     },
     body: JSON.stringify({url: data }),
-    });
+    })
 
     try {
         const apiAnswer = await resp.json()
-        console.log(apiAnswer);
+        console.log('JSON:', apiAnswer)
         return apiAnswer;
 
     } catch(error) {
@@ -48,17 +45,21 @@ const fetchData = async(textUrl = {},  data = {}) => {
     }
 }
 
-// translate api output
-function polarity (result) {
-  const answers = {
-    'P+': 'strong positive',
-    'NEU': 'neutral',
-    'P': 'positive',
-    'N': 'negative',
-    'N+': 'strong negative',
-    'default': 'without sentiment'
-  };
-  return (answers[result] || answers['default']);
+function polarity(result) {
+    if (result === 'P+') {
+        result = 'Strong Positive';
+    } else if (result === 'P') {
+        result = 'Positive';
+    } else if (result === 'NEU') {
+        result = 'Neutral';
+    } else if (result === 'N') {
+        result = 'Negative';
+    } else if (result === 'N+') {
+        result = 'Strong Negative';
+    } else {
+        result = 'without Sentiment';
+    }
+    return result;
 }
 
 export { handleSubmit }
